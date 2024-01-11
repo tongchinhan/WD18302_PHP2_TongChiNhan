ba4833487ecd213bfaab3ab33bea66137b1254b1
tongchinhan <tongchinhan76@gmail.com>
2024-01-11 16:36:57 +0700

    lab2

--- lab1.local/Bai3/Controller/Controller.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include '../Model/MoDel.php';
-    $email = $_POST['email'] ?? '';
-    $user = get_user($email);
-    
-    include '../Views/views.php';
-?>
--- lab1.local/Bai4/Controller/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../Model/MoDel.php';
-
-// Gọi hàm để lấy dữ liệu từ cơ sở dữ liệu
-$userData = get_all_users();
-
-// Include file HTML hiển thị
-include '../Views/views.php';
-?>
